df750f00258536e1ce57bfae46794185b706500c
wacax <wacax@hotmail.com>
2014-09-09 16:14:21 -0500

    added memory plus neural networks

--- CriteoMain.py
+++ CriteoMain.py
@@ -2,9 +2,13 @@ __author__ = 'wacax'
 
 #libraries
 from os import getcwd, chdir, system
-from wabbit_wappa import *
+from numpy import exp
+
 #Add user defined functions
 from csv2vw import csv_to_vw
+
+def sigmoid(x):
+    return 1 / (1 + exp(-x))
 
 #directories; change directories accordingly
 wd = '/home/wacax/Wacax/Kaggle/criteoLabs/CRITEO Display Advertising Challenge/'
@@ -18,16 +22,31 @@ if getcwd() + '/' != wd:
 csv_to_vw(dataDir + 'train.csv', dataDir + 'train.vw', train=True)
 csv_to_vw(dataDir + 'test.csv', dataDir + 'test.vw', train=False)
 
-#Use wabbit_wappa for training
-#vw = VW(loss_function='logistic')
-#print vw.command
-
 #Use vowpal wabbit directly from command line for training
-#changew working directory to data directory to make the operations there
-chdir(dataDir)
+#vw hypersearch
+# for a logistic loss train-set:
+#system('vw-hypersearch 1e-10 1 vw --l1 % ' + dataDir + 'train.vw')
 
+# Find the learning-rate resulting in the lowest average loss
+# for a logistic loss train-set:
+#system('vw-hypersearch 0.1 100 vw --loss_function logistic --learning_rate % train.dat')
+
+#MODELING
+#Logistic Regression with l1 regularization
 #Training VW:
-system('vw train.vw -f model.vw --loss_function logistic')
-
+system('vw ' + dataDir + 'train.vw -f ' + dataDir + 'modell1.vw --loss_function logistic --passes 20 -q ii -b 28')
 #Testing VW:
-system('vw test.vw -t -i model.vw -p preds.txt')
+system('vw ' + dataDir + 'test.vw -t -i ' + dataDir + 'modell1.vw -p ' + dataDir + 'logRegl1.txt')
+
+#Neural Networks
+#Training VW:
+system('vw ' + dataDir + 'train.vw -f ' + dataDir + 'NN.vw --nn 100 --passes 20 -q ii -b 28')
+#Testing VW:
+system('vw ' + dataDir + 'test.vw -t -i ' + dataDir + 'NN.vw -p ' + dataDir + 'NN100.txt')
+
+with open(dataDir + 'PredictionIII.csv', 'wb') as outfile:
+    outfile.write('Id,Predicted\n')
+#    for line in open(dataDir + 'logRegl1.txt'):
+    for line in open(dataDir + 'NN100.txt'):
+        row = line.strip().split(" ")
+        outfile.write("%s,%f\n"%(row[1], sigmoid(float(row[0]))))
